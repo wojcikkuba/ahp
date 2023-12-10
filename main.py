3a0dc9fbf09ca42078558b74f7efe96f14fbd485
wojcikkuba <wojcikjakub@int.pl>
2023-12-10 21:33:11 +0100

    Calculate consistency ratio

--- main.py
+++ main.py
@@ -10,25 +10,39 @@ def calculate():
         data = request.get_json()
         answers = data['answers']
 
-        # Grupowanie odpowiedzi według kryteriów
+        # Grupowanie odpowiedzi według kryteriów i tworzenie macierzy AHP
         grouped_answers = {}
+        grouped_matrices = {}
         for answer in answers:
             criterion = answer['criterion']
             if criterion not in grouped_answers:
                 grouped_answers[criterion] = []
             grouped_answers[criterion].append(answer)
 
-        results = {}
         for criterion, criterion_answers in grouped_answers.items():
-            # Tworzenie macierzy AHP dla danego kryterium
             matrix = create_ahp_matrix_from_answers(criterion_answers)
-            # Obliczanie wag priorytetowych
+            grouped_matrices[criterion] = matrix
+
+        # Obliczenia dla każdego kryterium
+        results = {}
+        consistency_ratios = {}
+        for criterion, matrix in grouped_matrices.items():
             weights = calculate_priority_vector(matrix)
             results[criterion] = weights.tolist()
 
+            # Obliczanie wskaźnika spójności
+            eigenvalues, _ = np.linalg.eig(matrix)
+            CR = calculate_consistency_ratio(matrix, eigenvalues)
+            consistency_ratios[criterion] = CR
+
+        # Sprawdzanie, czy którykolwiek CR przekracza próg 0.1
+        inconsistent_criteria = [criterion for criterion, CR in consistency_ratios.items() if CR > 0.1]
+        if inconsistent_criteria:
+            return jsonify({"error": "Inconsistent answers for criteria: " + ", ".join(inconsistent_criteria)}), 400
+
         # Synteza wyników
         final_scores = np.sum([np.array(weights) for weights in results.values()], axis=0)
-        best_variant = int(np.argmax(final_scores)) + 1  # Konwersja na typ podstawowy Pythona
+        best_variant = int(np.argmax(final_scores)) + 1
 
         # Odczyt i zapis do pliku JSON
         file_name = 'results.json'
@@ -36,31 +50,29 @@ def calculate():
 
         try:
             # Odczytanie istniejących danych z pliku, jeśli plik istnieje
-            try:
-                with open(file_name, 'r') as file:
-                    results_data = json.load(file)
-            except FileNotFoundError:
-                results_data = {}
+            with open(file_name, 'r') as file:
+                results_data = json.load(file)
+        except FileNotFoundError:
+            results_data = {}
 
-            # Dodanie nowych wyników
-            results_data[user_name] = {
-                'best_variant': best_variant,
-                'results': {criterion: np.array(weights).tolist() for criterion, weights in results.items()},
-                'scores': final_scores.tolist()
-            }
+        # Dodanie nowych wyników
+        results_data[user_name] = {
+            'best_variant': best_variant,
+            'results': results,
+            'scores': final_scores.tolist(),
+            'consistency_ratios': consistency_ratios
+        }
 
-            # Zapisanie zmodyfikowanych danych do pliku
-            with open(file_name, 'w') as file:
-                json.dump(results_data, file, indent=4)
-
-        except Exception as e:
-            return jsonify({"error": f"Error writing to file: {str(e)}"}), 500
+        # Zapisanie zmodyfikowanych danych do pliku
+        with open(file_name, 'w') as file:
+            json.dump(results_data, file, indent=4)
 
         # Zwracanie wyników jako odpowiedź API
         return jsonify({
             'best_variant': best_variant,
-            'scores': final_scores.tolist(),  # Konwersja na listę typów podstawowych
-            'results': {criterion: np.array(weights).tolist() for criterion, weights in results.items()}
+            'scores': final_scores.tolist(),
+            'results': results,
+            'consistency_ratios': consistency_ratios
         })
 
     except ValueError:
@@ -93,6 +105,15 @@ def calculate_priority_vector(matrix):
 
     # Normalizacja wektora własnego do uzyskania wag
     return max_eigenvector / np.sum(max_eigenvector)
+
+
+def calculate_consistency_ratio(matrix, eigenvalues):
+    n = matrix.shape[0]
+    lambda_max = np.real(np.max(eigenvalues))
+    ci = (lambda_max - n) / (n - 1)  # Wskaźnik spójności (Consistency Index)
+    ri = 0.58 if n == 3 else 0  # Losowy wskaźnik spójności dla macierzy 3x3
+    cr = ci / ri if ri != 0 else 0  # Współczynnik spójności
+    return cr
 
 
 if __name__ == '__main__':
