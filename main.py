96d23e1d97caa83161704bb4b6c85e70d65c46c0
wojcikkuba <wojcikjakub@int.pl>
2023-12-11 01:24:48 +0100

    new json

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def calculate():
     try:
         data = request.get_json()
+        user_name = data.get('userName', 'Unknown')
+        kategorie = data.get('kategorie', [])
+        warianty = data.get('warianty', [])
         answers = data['answers']
 
         # Grupowanie odpowiedzi według kryteriów i tworzenie macierzy AHP
@@ -43,36 +46,47 @@ def calculate():
         final_scores = np.sum([np.array(weights) for weights in results.values()], axis=0)
         best_variant = int(np.argmax(final_scores)) + 1
 
+        # Przygotowanie struktury JSON z wynikami
+        survey_results = []
+
+        for criterion in kategorie:
+            survey_result = {
+                'ankieta': "Ankieta 1",  # Możesz dostosować to pole na podstawie danych wejściowych
+                'kategorie': [criterion],
+                'warianty': warianty,
+                'najlepszy_wariant': {criterion: best_variant},
+                'wyniki': []
+            }
+
+            for answer in answers:
+                user_result = {
+                    'uzytkownik': user_name,
+                    'oceny': {criterion: results[criterion]},
+                    'is_consistent': consistency_ratios[criterion] <= 0.1
+                }
+                survey_result['wyniki'].append(user_result)
+
+            survey_result['scores'] = final_scores.tolist()
+            survey_results.append(survey_result)
+
         # Odczyt i zapis do pliku JSON
         file_name = 'results.json'
-        survey_result = {
-            'best_variant': best_variant,
-            'results': results,
-            'scores': final_scores.tolist(),
-            'consistency_ratios': consistency_ratios,
-            'is_consistent': not inconsistent_criteria
-        }
-
         try:
-            # Odczytanie istniejących danych z pliku, jeśli plik istnieje
             with open(file_name, 'r') as file:
                 results_data = json.load(file)
         except FileNotFoundError:
-            results_data = []
+            results_data = {'najlepszy_wariant': None, 'Ankiety': []}
 
-        # Dodanie wyniku ankiety do listy
-        results_data.append(survey_result)
+        results_data['Ankiety'].extend(survey_results)
 
-        # Zapisanie zmodyfikowanych danych do pliku
+        # Agregacja wyników
+        najlepszy_wariant = aggregate_results(results_data['Ankiety'])
+        results_data['najlepszy_wariant'] = najlepszy_wariant
+
         with open(file_name, 'w') as file:
             json.dump(results_data, file, indent=4)
 
-        if inconsistent_criteria:
-            print(f"Inconsistent answers for criteria: {', '.join(inconsistent_criteria)}")
-            return jsonify({"error": "Inconsistent answers for criteria: " + ", ".join(inconsistent_criteria)}), 400
-
-        # Zwracanie wyników jako odpowiedź API
-        return jsonify(survey_result)
+        return jsonify(survey_results)
 
     except ValueError:
         return jsonify({"error": "Invalid data format"}), 400
@@ -113,6 +127,31 @@ def calculate_consistency_ratio(matrix, eigenvalues):
     ri = 0.58 if n == 3 else 0  # Losowy wskaźnik spójności dla macierzy 3x3
     cr = ci / ri if ri != 0 else 0  # Współczynnik spójności
     return cr
+
+
+def aggregate_results(surveys):
+    suma_wag = {}
+    liczba_wag = {}
+
+    # Agregacja wyników
+    for ankieta in surveys:
+        for kategoria, wagi in ankieta['results'].items():
+            if kategoria not in suma_wag:
+                suma_wag[kategoria] = np.zeros(len(wagi))
+                liczba_wag[kategoria] = 0
+
+            mnoznik = 0.5 if not ankieta['is_consistent'] else 1
+            suma_wag[kategoria] += np.array(wagi) * mnoznik
+            liczba_wag[kategoria] += mnoznik
+
+    # Obliczanie średnich ważonych i wybór najlepszego wariantu
+    najlepszy_wariant = {}
+    for kategoria, suma in suma_wag.items():
+        srednia = suma / liczba_wag[kategoria]
+        najlepszy_wariant[kategoria] = np.argmax(srednia) + 1  # +1, ponieważ indeksowanie zaczyna się od 0
+
+    return najlepszy_wariant
+
 
 
 if __name__ == '__main__':
